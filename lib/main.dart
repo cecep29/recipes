4b923a7f43bb6c646b1c5f39ed0fc2ee38fb77c0
cecep29 <cecepefendi.c2p@gmail.com>
2023-01-08 22:22:42 +0700

    display recipe with listview builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'recipe.dart';
+
 void main() {
   runApp(const RecipeApp());
 }
@@ -37,7 +39,11 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        child: Container(),
+        child: ListView.builder(
+            itemCount: Recipe.samples.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Text(Recipe.samples[index].label);
+            }),
       ),
     );
   }
